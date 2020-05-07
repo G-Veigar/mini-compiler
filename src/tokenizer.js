411def7aa204453b15444e3be21238dd2d91af80
gaoge2 <gaoge2@vipkid.com.cn>
2020-05-07 14:01:46 +0800

    save

---
+++ src/tokenizer.js
@@ -0,0 +1,11 @@
+// 词法分析
+function tokenizer(codeStr) {
+    codeStr = codeStr.trim()
+    let codeLength = codeStr.length
+    let tokens = []
+    for(let i = 0; i < codeLength; i++) {
+        let currentLetter = codeStr[i]
+    }
+}
+
+module.exports = tokenizer
